c4ebc76ca7ec81ae2bd5948fae5df0baabf942b8
mradams276 <kyle.adams276@gmail.com>
2016-09-12 08:47:47 -0600

    3rd question

--- src/stupid/controller/StupidController.java
+++ src/stupid/controller/StupidController.java
@@ -26,5 +26,14 @@ public class StupidController
 		System.out.println("How was your day?");
 		String input = inputReader.nextLine();
 		System.out.println("Wow, you said: " + input + " Oh wait, I don't care");
+		
+		System.out.println("Sorry, that was very rude of me.  I want to get to know you a little better.  What is your favorite type of food?");
+		String foodInput = inputReader.nextLine();
+		System.out.println("You said you enjoy " + foodInput + ", I guess that's pretty good");
+		
+		System.out.println("So now that we are friends and everything, mind telling me your credit card number for absolutely no reason at all?");
+		Double cardInput = inputReader.nextDouble();
+		System.out.println(" MWHAHAHAHA *cough* I meant, uh, thanks!");
+		
 	}
 }
